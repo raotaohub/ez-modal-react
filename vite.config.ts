39ac125d860405534eb4ec760d2e2bde7bc1ed40
raotaohub <raotao@outlook.com>
2023-06-19 00:58:31 +0800

    chore: vite.config.ts remove invalid options

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   root: './demo',
   server: {
     port: 6060,
   },
-  plugins: [
-    react(),
-    svgr({
-      exportAsDefault: false,
-    }),
-  ],
+  plugins: [react()],
 });
